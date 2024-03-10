3f59ef9bd823dcd9744c46055e31efd0432498cd
yuni <hys990730@gmail.com>
2024-03-10 12:35:47 +0900

    feat: 로그인 상태에 따른 라우트 설정 - 삼항 연산자 사용

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { useSelector } from "react-redux";
 
-// Adjusted the import for Dashboard to reflect a common project structure
 import Login from './pages/Login/Login';
-import Dashboard from './pages/Dashboard/Dashboard'; // Corrected the path
+import Dashboard from './pages/Dashboard/Dashboard'; 
 
 import AdminNavbar from './components/AdminNavbar';
 import UserManagementPage from './components/UserManagementPage';
@@ -14,40 +13,24 @@ import CoordinateLookManagementPage from './components/CoordinateLookManagementP
 import ItemManagementPage from './components/ItemManagementPage';
 
 function App() {
-  const user = useSelector(state => state.user);
+  const { isLogin } = useSelector(state => state.user);
 
   return (
     <Router>
       <div>
-        {/* 네비게이션 바 */}
+
         <AdminNavbar />
-
+        
         <Routes>
-          {/* Redirect based on user login status */}
-          <Route path="/" element={user.isLogin ? <Navigate to="/admin/dashboard" /> : <Navigate to="/admin/login" />} />
-          
+          <Route path="/" element={isLogin ? <Navigate to="/admin/dashboard" /> : <Navigate to="/admin/login" />} />
           <Route path="/admin/login" element={<Login />} />
-          <Route path="/admin/dashboard" element={<Dashboard />} />
+          <Route path="/admin/dashboard" element={isLogin ? <Dashboard /> : <Navigate to="/admin/login" />} />
 
-          {/* Protected Routes: These routes are accessible only if the user is logged in */}
-          {user.isLogin ? (
-            <>
-              <Route path="/user-management" element={<UserManagementPage />} />
-              <Route path="/style-point-management" element={<StylePointManagementPage />} />
-              <Route path="/brand-management" element={<BrandManagementPage />} />
-              <Route path="/coordinate-look-management" element={<CoordinateLookManagementPage />} />
-              <Route path="/item-management" element={<ItemManagementPage />} />
-            </>
-          ) : (
-            /* Redirect to login page if the user is not logged in and trying to access protected routes */
-            <>
-              <Route path="/user-management" element={<Navigate to="/admin/login" />} />
-              <Route path="/style-point-management" element={<Navigate to="/admin/login" />} />
-              <Route path="/brand-management" element={<Navigate to="/admin/login" />} />
-              <Route path="/coordinate-look-management" element={<Navigate to="/admin/login" />} />
-              <Route path="/item-management" element={<Navigate to="/admin/login" />} />
-            </>
-          )}
+          <Route path="/user-management" element={isLogin ? <UserManagementPage /> : <Navigate to="/admin/login" />} />
+          <Route path="/style-point-management" element={isLogin ? <StylePointManagementPage /> : <Navigate to="/admin/login" />} />
+          <Route path="/brand-management" element={isLogin ? <BrandManagementPage /> : <Navigate to="/admin/login" />} />
+          <Route path="/coordinate-look-management" element={isLogin ? <CoordinateLookManagementPage /> : <Navigate to="/admin/login" />} />
+          <Route path="/item-management" element={isLogin ? <ItemManagementPage /> : <Navigate to="/admin/login" />} />
         </Routes>
       </div>
     </Router>
