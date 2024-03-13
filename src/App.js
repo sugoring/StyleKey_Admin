329941c3826ebf94920bdbea728c95ca5c65d39a
YUNI Heo <ys8973@kangwon.ac.kr>
2024-03-13 16:49:14 +0900

    Revert "Feat redux - slice 구현"

--- src/App.js
+++ src/App.js
@@ -1,41 +1,24 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Selectedbar from "./components/Selectedbar"; 
-
-import StylePointsPage from "./features/stylePoints/StylePointsPage";
-import StylePointDetails from "./features/stylePoints/StylePointDetails";
-import StylePointUpdate from "./features/stylePoints/StylePointUpdate";
-import BrandDetails from "./features/brands/BrandDetails";
-import BrandCreate from "./features/brands/BrandCreate";
-import BrandUpdate from "./features/brands/BrandUpdate";
-import CoordinateLookDetails from "./features/coordinateLooks/CoordinateLookDetails";
-import CoordinateLookCreate from "./features/coordinateLooks/CoordinateLookCreate";
-import CoordinateLookUpdate from "./features/coordinateLooks/CoordinateLookUpdate";
-import './App.css'; 
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <Router>
-      <>
-        <Selectedbar/>
-        <Navbar />
-        
-        <Routes>
-          <Route path="/" element={<StylePointsPage />} />
-          <Route path="/stylepoint/:id" element={<StylePointDetails />} />
-          <Route path="/stylepoint/:id/update" element={<StylePointUpdate />} />
-
-          <Route path="/brand/:id" element={<BrandDetails />} />
-          <Route path="/brand/create" element={<BrandCreate />} />
-          <Route path="/brand/:id/update" element={<BrandUpdate />} />
-
-          <Route path="/coordinate-look/:id" element={<CoordinateLookDetails />} />
-          <Route path="/coordinate-look/create" element={<CoordinateLookCreate />} />
-          <Route path="/coordinate-look/:id/update" element={<CoordinateLookUpdate />} />
-        </Routes>
-      </>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
